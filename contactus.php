c87dae31b2ebf419b48f937429f4731d5b02b806
gjylimekaj <gjylimekaj1@gmail.com>
2020-12-28 15:40:22 +0100

    contact responsive

--- contactus.php
+++ contactus.php
@@ -1,3 +1,32 @@
+<?php   
+    require 'includes/dbconnect.php';
+?>
+
+<?php
+if(isset($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+    $toEmail = "gjylimekaj1@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+
+    $sql = 'INSERT INTO tblcontact(user_name, user_email, subject, content) VALUES (:name, :email, :subject, :content)';
+        $query = $pdo->prepare($sql);
+        $query->bindParam('name', $name);
+        $query->bindParam('email', $email);
+        $query->bindParam('subject', $subject);
+        $query->bindParam('content', $kategoria);
+        $query->execute();
+    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,11 +53,20 @@
             <div class="contact-left"></div>
             <div class="contact-right">
                 <h2>Contact Us</h2>
-                <input type="text" class="field" placeholder="Your Name">
-                <input type="email" class="field" placeholder="Your Email">
-                <input type="text" class="field" placeholder="Your Phone">
-                <textarea class="field area" placeholder="Message"></textarea>
-                <button class="contact-btn">Send</button>
+                <input type="text" name = "name" class="field" placeholder="Your Name">
+                <input type="email" name = "email" class="field" placeholder="Your Email">
+                <input type="text" class="field" name = "subject" placeholder="Subject">
+                <textarea class="field area" name = "content" placeholder="Message"></textarea>
+                <button class="contact-btn" name = "send">Send</button>
+                <div id="statusMessage"> 
+                        <?php
+                        if (! empty($message)) {
+                            ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
             </div>
         </div>
         </form>
